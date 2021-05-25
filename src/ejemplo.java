2bc33e1e623e0f5796fa45ae1f3ea20af0c99ea5
Jesus272 <jesusguadaluperdz@hotmail.com>
2021-05-24 18:48:46 -0600

    Este es un ejemplo de Java los demas son archivos

---
+++ src/ejemplo.java
@@ -0,0 +1,11 @@
+public class ejemplo {
+
+        public static void (String[]args){
+            int[] even_numbers = new int[]{ 1,2,3,4,5,6,7,8,9,10};
+            for (int number : even_numbers){
+                System.out.print1n("Number "+number + "...");
+            }
+            return;
+        }
+}
+
